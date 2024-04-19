b4e53fca995f9b09f0dbbf8fefca0d8cc39deabc
Mohamed Ashraf <mohamed.code.13@gmail.com>
2024-04-19 15:27:02 +0200

    Initialize the private fields and the insert method for single key.

--- hash_tables/HashTableWithNSpace.java
+++ hash_tables/HashTableWithNSpace.java
@@ -1,17 +1,40 @@
 package hash_tables;
 
-public class HashTableWithNSpace<T> implements IHashTable{
+public class HashTableWithNSpace<T> implements IHashTable {
 
     // Fields HERE
+    private HashTableWithN2Space<T>[] table;
+    private Matrix matrix;
+    private int hashCount;
+    private int max_size;
+    private int cur_size;
 
-    @Override
-    public void insert(Object key) {
-
+    public HashTableWithNSpace() {
+        cur_size = 0;
+        max_size = 10;
+        hashCount = 0;
+        matrix = new Matrix(max_size);
+        table = new HashTableWithN2Space[max_size];
     }
 
     @Override
-    public void delete(Object key) {
+    public boolean insert(Object key) {
+        int index = matrix.getIndex(key) % max_size;
+        if (table[index] == null) {
+            cur_size++;
+            table[index] = new HashTableWithN2Space<>();
+        }
 
+        boolean res = table[index].insert(key);
+        if (needs_to_rehash())
+            rehash();
+
+        return res;
+    }
+
+    @Override
+    public boolean delete(Object key) {
+        return false;
     }
 
     @Override
@@ -20,12 +43,20 @@ public class HashTableWithNSpace<T> implements IHashTable{
     }
 
     @Override
-    public boolean batchInsert(Object[] keys) {
-        return false;
+    public int batchInsert(Object[] keys) {
+        return 0;
     }
 
     @Override
-    public boolean batchDelete(Object[] keys) {
-        return false;
+    public int batchDelete(Object[] keys) {
+        return 0;
+    }
+
+    private boolean needs_to_rehash() {
+        return (1.0 * cur_size / max_size) >= 0.5;
+    }
+
+    private void rehash() {
+
     }
 }
