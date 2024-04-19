da90173d6843d844a209b54e6ca8eeaa1b7f0b87
porvah <3msabdullah@gmail.com>
2024-04-19 14:33:28 +0200

    repushing failed commit

--- hash_tables/HashTableWithN2Space.java
+++ hash_tables/HashTableWithN2Space.java
@@ -1,32 +1,106 @@
 package hash_tables;
 
+import java.util.ArrayList;
+
 public class HashTableWithN2Space<T> implements IHashTable{
 
 
     // Fields HERE
+    ArrayList<T> table;
+    int n;
+    Matrix matrix;
+    int size;
+    int elements;
+    int hashCount;
+    HashTableWithN2Space(){
+        n = 10;
+        elements = 0;
+        size = 2*n*n;
+        table = new ArrayList<T>(size);
+        table.set(size-1, null);
+        matrix = new Matrix(size);
+        hashCount = 1;
+    }
 
     @Override
-    public void insert(Object key) {
+    public boolean insert(Object key) {
+        int index = matrix.getIndex(key) % size;
+        if(table.get(index) == null){
+            table.set(index, (T)key);
+            elements++;
+            return true;
+        }else{
+            if(!key.equals(table.get(index))){
+                //collision occurred
+                if(elements+1 > n){
+                    //rehash with new sizes
+                    ArrayList<T> newElements = new ArrayList<T>();
+                    newElements.add((T)key);
+                    rehashSameSize(newElements);
+                }else{
+                    //rehash with the same size
+                }
+                return true;
+            }else{
+                return false;
+            }
+        }
+
+    }
+    private void rehashSameSize(ArrayList<T> newElements){
+
+        boolean collision = true;
+        while(collision) {
+            int addedElements = 0;
+            hashCount++;
+            // setting up a new table
+            collision = false;
+            matrix = new Matrix(size);
+            ArrayList<T> newTable = new ArrayList<>(size);
+            newTable.set(size - 1, null);
+            for(int i = 0; i < table.size(); i++){
+                if(table.get(i) != null){
+                    int index = matrix.getIndex(table.get(i)) % size;
+                    Object key = table.get(i);
+                    if(newTable.get(index) == null){
+
+                    }
+                }
+            }
+            if(collision) break;
+        }
+    }
+    private void rehash(){
 
     }
 
     @Override
-    public void delete(Object key) {
-
+    public boolean delete(Object key) {
+        int index = matrix.getIndex(key) % size;
+        if(table.get(index) != null){
+            table.set(index, null);
+            elements--;
+            return true;
+        }
+        return false;
     }
 
     @Override
     public boolean search(Object key) {
+        int index = matrix.getIndex(key) % size;
+        if(table.get(index) != null) {
+            if(key.equals(table.get(index))) return true;
+        }
         return false;
     }
 
     @Override
-    public boolean batchInsert(Object[] keys) {
-        return false;
+    public int batchInsert(Object[] keys) {
+        return 0;
     }
 
     @Override
-    public boolean batchDelete(Object[] keys) {
-        return false;
+    public int batchDelete(Object[] keys) {
+        return 0;
     }
 }
--- hash_tables/IHashTable.java
+++ hash_tables/IHashTable.java
@@ -2,11 +2,11 @@ package hash_tables;
 
 public interface IHashTable<T> {
 
-    void insert(T key);
-    void delete(T key);
+    boolean insert(T key);
+    boolean delete(T key);
     boolean search(T key);
 
-    boolean batchInsert(T[] keys);
+    int batchInsert(T[] keys);
 
-    boolean batchDelete(T[] keys);
+    int batchDelete(T[] keys);
 }
